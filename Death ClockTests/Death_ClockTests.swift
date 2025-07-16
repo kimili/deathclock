6a6cb7ca75ef31994147b1df698edf13cd0649ba
Michael Bester <michael@kimili.com>
2025-07-16 15:33:25 -0400

    Initial Commit

---
+++ Death ClockTests/Death_ClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  Death_ClockTests.swift
+//  Death ClockTests
+//
+//  Created by Michael Bester on 7/16/25.
+//
+
+import Testing
+@testable import Death_Clock
+
+struct Death_ClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
